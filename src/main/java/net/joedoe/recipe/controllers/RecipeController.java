e0ed385ab010a0197d17133d8d5575628c4dafb9
j-o-e-d-o-e <d_joe@gmx.net>
2018-06-26 21:22:44 +0200

    validation added

--- src/main/java/net/joedoe/recipe/controllers/RecipeController.java
+++ src/main/java/net/joedoe/recipe/controllers/RecipeController.java
@@ -3,20 +3,25 @@ package net.joedoe.recipe.controllers;
 import lombok.extern.slf4j.Slf4j;
 import net.joedoe.recipe.commands.RecipeCommand;
 import net.joedoe.recipe.domains.Recipe;
-import net.joedoe.recipe.exceptions.NotFoundException;
 import net.joedoe.recipe.services.CategoryService;
 import net.joedoe.recipe.services.IRecipeService;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
 public class RecipeController {
     private IRecipeService<Recipe> recipeService;
     private CategoryService categoryService;
+
+    private static final String RECIPE_FORM_URL = "recipe/recipe-form";
 
     public RecipeController(IRecipeService<Recipe> recipeService, CategoryService categoryService) {
         this.recipeService = recipeService;
@@ -28,7 +33,7 @@ public class RecipeController {
         log.debug("RecipeController: newRecipe()");
         model.addAttribute("recipe", new Recipe());
         model.addAttribute("categories", categoryService.findAll());
-        return "recipe/recipe-form";
+        return RECIPE_FORM_URL;
     }
 
     @GetMapping("/recipe/{id}/show")
@@ -41,7 +46,7 @@ public class RecipeController {
     public String getUpdateViewRecipe(@PathVariable String id, Model model) {
         model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
         model.addAttribute("categories", categoryService.findAll());
-        return "recipe/recipe-form";
+        return RECIPE_FORM_URL;
     }
 
     @GetMapping("/recipe/{id}/delete")
@@ -51,8 +56,12 @@ public class RecipeController {
         return "redirect:/";
     }
 
-    @PostMapping("/recipe")
-    public String saveOrUpdateRecipe(@ModelAttribute RecipeCommand command) {
+    @PostMapping("/recipe") //@ModelAttribute needs to be named "recipe" according to obj in template "recipe-form"
+    public String saveOrUpdateRecipe(@Valid @ModelAttribute("recipe") RecipeCommand command, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()){
+            bindingResult.getAllErrors().forEach(error -> log.debug(error.toString()));
+            return RECIPE_FORM_URL;
+        }
         log.debug("RecipeController: saveOrUpdateIngredient()");
         RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
         return "redirect:/recipe/" + savedCommand.getId() + "/show";
